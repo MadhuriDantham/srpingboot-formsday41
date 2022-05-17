1ffa6620811158cfaae22153e91b2eafc32c57a1
madhuridantham <danthammadhuri5@gmail.com>
2022-05-17 11:26:19 +0530

    done

---
+++ srpingboot-formsday41/src/main/java/com/jobiak/mvcforms/SrpingbootFormsday41Application.java
@@ -0,0 +1,13 @@
+package com.jobiak.mvcforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingbootFormsday41Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingbootFormsday41Application.class, args);
+	}
+
+}
---
+++ srpingboot-formsday41/src/main/java/com/jobiak/mvcforms/model/Account.java
@@ -0,0 +1,39 @@
+package com.jobiak.mvcforms.model;
+
+public class Account {
+	
+	private Long userId;
+	private String name;
+	private double bal;
+	
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getBal() {
+		return bal;
+	}
+
+	public void setBal(double bal) {
+		this.bal = bal;
+	}
+	
+	
+
+}
